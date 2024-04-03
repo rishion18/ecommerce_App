848b22535da0172ae84cf96eb9ae9bc52033cece
Rishi Raj <kvkv.raj83@gmail.com>
2024-04-03 11:30:20 +0530

    env update 3

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -51,8 +51,8 @@ userSchema.methods = {
             userRole: this.role,
             userName:this.username
         };
-        const accessToken = jwt.sign(payload, 'thisisasamplepasswordforjwt', { expiresIn: '2d' });
-        const refreshToken = jwt.sign(payload, 'thisisasamplepasswordforjwt', { expiresIn: '2d' });        
+        const accessToken = jwt.sign(payload, process.env.Jwt_secret, { expiresIn: '2d' });
+        const refreshToken = jwt.sign(payload, process.env.Jwt_secret , { expiresIn: '2d' });        
 
         return {accessToken , refreshToken};
     }
